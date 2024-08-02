2e80fc321ffd4490a5fb437ae7544833748ada7f
TheNewAutonomy <darren@ourfamily.io>
2024-08-02 16:03:46 +0100

    Added Production NFT

--- contracts/JeskeiProduction.sol
+++ contracts/JeskeiProduction.sol
@@ -1,108 +1,98 @@
+// contracts/JeskeiAsset.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "./JeskeiAsset.sol";
-import "./JeskeiStudio.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract JeskeiProduction {
+contract Production is ERC721URIStorage, Ownable {
+    enum FilmRatingEnum { G, PG, PG13, R, NC17 }
 
-    enum AgeRating{ G, PG, PG13, R, NC17 }
-
-    struct Production {
+    struct ProductionAssets {
         bytes32 id;
-        string name;
-        string description;
-        AgeRating ageRating;
-        bytes32[] assets;
-        bytes32 studio;
+        address assetAddress; // 0x000.. for assets without an address
         uint256 view_Wei;
     }
 
-    Production[] private productions;
-    mapping(bytes32 => uint) private productionIndex;
-    mapping(bytes32 => uint) private fees;
+    uint256 private _nextTokenId;
+    string private _description;
+    FilmRatingEnum private _filmRating;
+    string[] private _tags;
+    uint256 private _view_Wei; // Must be at least the sum of all listed assets
 
-    modifier onlyExistingProduction(bytes32 productionId) {
-        require(productionIndex[productionId] != 0 || (productions.length > 0 && productions[0].id == productionId), "Production does not exist");
-        _;
+    ProductionAssets[] private assets;
+    
+    event ProductionCreated(uint256 tokenId, address indexed creator, string tokenURI, string description, FilmRatingEnum filmRating, uint256 viewWei, string[] tags);
+    event DescriptionUpdated(string oldDescription, string newDescription);
+    event FilmRatingUpdated(FilmRatingEnum oldRating, FilmRatingEnum newRating);
+    event TagsUpdated(string[] oldTags, string[] newTags);
+    event ViewWeiUpdated(uint256 oldViewWei, uint256 newViewWei);
+
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) Ownable(msg.sender) {
+        transferOwnership(msg.sender);
     }
 
-    function createProduction(bytes32 studio, string memory name, string memory description, AgeRating age_Rating) public {
-        bytes32 id = keccak256(abi.encodePacked(name, description, age_Rating, studio, blockhash(block.number - 1)));
-        Production memory newProduction;
-        newProduction.id = id;
-        newProduction.name = name;
-        newProduction.ageRating = age_Rating;
-        newProduction.studio = studio;
-        productions.push(newProduction);
-        productionIndex[id] = productions.length - 1;
+    function createProduction(
+        address productionCreator_,
+        string memory tokenURI_,
+        string memory description_,
+        FilmRatingEnum filmRating_,
+        uint256 viewWei_,
+        string[] memory tags_
+    ) public onlyOwner returns (uint256) {
+        require(viewWei_ > 0, "View cost must be greater than zero");
+
+        uint256 tokenId = _nextTokenId++;
+        _mint(productionCreator_, tokenId);
+        _setTokenURI(tokenId, tokenURI_);
+        _description = description_;
+        _filmRating = filmRating_;
+        _tags = tags_;
+        _view_Wei = viewWei_;
+        
+        emit ProductionCreated(tokenId, productionCreator_, tokenURI_, description_, filmRating_, viewWei_, tags_);
+
+        return tokenId;
     }
 
-    function deleteProduction(bytes32 productionId) public onlyExistingProduction(productionId) {
-        uint index = productionIndex[productionId];
-        uint lastIndex = productions.length - 1;
-
-        if (index != lastIndex) {
-            productions[index] = productions[lastIndex];
-            productionIndex[productions[index].id] = index;
-        }
-
-        productions.pop();
-        delete productionIndex[productionId];
+    // Getter functions to make the properties publicly viewable
+    function getDescription() public view returns (string memory) {
+        return _description;
     }
 
-    function transferProduction(bytes32 productionId, bytes32 targetStudio) public onlyExistingProduction(productionId) {
-        uint index = productionIndex[productionId];
-        productions[index].studio = targetStudio;
+    function getFilmRating() public view returns (FilmRatingEnum) {
+        return _filmRating;
     }
 
-    function addAsset(bytes32 productionId, bytes32 assetId) public onlyExistingProduction(productionId) {
-        uint index = productionIndex[productionId];
-        productions[index].assets.push(assetId);
+    function getTags() public view returns (string[] memory) {
+        return _tags;
     }
 
-    function addAssets(bytes32 productionId, bytes32[] memory assetIds) public onlyExistingProduction(productionId) {
-        uint index = productionIndex[productionId];
-        for (uint i = 0; i < assetIds.length; i++) {
-            productions[index].assets.push(assetIds[i]);
-        }
+    function getViewWei() public view returns (uint256) {
+        return _view_Wei;
     }
 
-    function removeAsset(bytes32 productionId, bytes32 assetId) public onlyExistingProduction(productionId) {
-        uint index = productionIndex[productionId];
-        uint assetIndex = 0;
-        bytes32[] storage assets = productions[index].assets;
-
-        for (uint i = 0; i < assets.length; i++) {
-            if (assets[i] == assetId) {
-                assetIndex = i;
-                break;
-            }
-        }
-
-        for (uint i = assetIndex; i < assets.length - 1; i++) {
-            assets[i] = assets[i + 1];
-        }
-        assets.pop();
+    // Owner-only functions to update the properties
+    function updateDescription(string memory newDescription) public onlyOwner {
+        emit DescriptionUpdated(_description, newDescription);
+        _description = newDescription;
     }
 
-    function getStudioProductions(bytes32 studioId) public view returns (bytes32[] memory) {
-        uint count = 0;
-        for (uint i = 0; i < productions.length; i++) {
-            if (productions[i].studio == studioId) {
-                count++;
-            }
-        }
-
-        bytes32[] memory studioProductions = new bytes32[](count);
-        uint index = 0;
-        for (uint i = 0; i < productions.length; i++) {
-            if (productions[i].studio == studioId) {
-                studioProductions[index] = productions[i].id;
-                index++;
-            }
-        }
-
-        return studioProductions;
+    function updateFilmRating(FilmRatingEnum newFilmRating) public onlyOwner {
+        emit FilmRatingUpdated(_filmRating, newFilmRating);
+        _filmRating = newFilmRating;
     }
+
+    function updateTags(string[] memory newTags) public onlyOwner {
+        emit TagsUpdated(_tags, newTags);
+        _tags = newTags;
+    }
+
+    function updateViewWei(uint256 newViewWei) public onlyOwner {
+        require(newViewWei > 0, "View cost must be greater than zero");
+        emit ViewWeiUpdated(_view_Wei, newViewWei);
+        _view_Wei = newViewWei;
+    }
+
+    // Additional functions to manage ProductionAssets can be added here
 }
